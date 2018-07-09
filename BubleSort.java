dd8a94e682f8d14461e0a0f08dec067ee6821e7d
masato0713 <33418059+masato0713@users.noreply.github.com>
2018-07-09 20:37:59 +0800

    Create BubleSort.java

---
+++ BubleSort.java
@@ -0,0 +1,15 @@
+/**
+  冒泡排序法
+*/
+public static void main(String[] args){
+  int[] arr = new int[]{6,4,7,3,1,8,9,2,5};
+  for(int i = 1;i < arr.length;i++){
+    for(int j = 1;j < arr.length;j++){
+      if(arr[j] < arr[j-1]){
+        int temp = arr[j-1];
+        arr[j-1] = arr[j];
+        a[j] = temp; //较大的数
+      }
+    }
+  }
+}
