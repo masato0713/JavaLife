33e0c2c3b8db7ea7e05f00d944f0ea87b4f29a07
masato0713 <33418059+masato0713@users.noreply.github.com>
2018-07-09 20:42:28 +0800

    Update BubleSort.java

--- BubleSort.java
+++ BubleSort.java
@@ -5,7 +5,7 @@ public class Bubble{
   public static void main(String[] args){
     int[] arr = new int[]{6,4,7,3,1,8,9,2,5};
     for(int i = 1;i < arr.length;i++){
-      for(int j = 1;j < arr.length;j++){
+      for(int j = 1;j < arr.length - 1;j++){
         if(arr[j] < arr[j-1]){
           int temp = arr[j-1];
           arr[j-1] = arr[j];
@@ -13,5 +13,8 @@ public class Bubble{
         }
       }
     }
+  }
+  for(int x = 0;x < arr.length;x++){
+    System.out.print(arr[x]+"\t");
   }
 }
